4296a148537bb4658c8aa5d6d29b6a1f13b1bb23
Highwind245 <joshuahughes1426@gmail.com>
2021-01-11 09:23:36 -0600

    Can now save and update entries

--- scripts/notes/NoteDataProvider.js
+++ scripts/notes/NoteDataProvider.js
@@ -19,7 +19,6 @@ export const getNotes = () => {
 
 export const saveNote = note => {
     let stringifiedObj = JSON.stringify(note)
-    debugger
     return fetch('http://localhost:8088/notes', {
         method: "POST",
         headers: {
